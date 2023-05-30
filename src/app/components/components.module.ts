01ffd9cabae1a29b299e230a1ad809ae7e635a22
zeotoni <zeotoni666@gmail.com>
2023-05-29 21:59:47 -0300

    Header component finalizado

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   imports: [
@@ -9,11 +10,13 @@ import { FooterComponent } from "./footer/footer.component";
   ],
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ]
 })
 export class ComponentsModule {}
